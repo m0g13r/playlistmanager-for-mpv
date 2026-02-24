036c127036989216bde3daa1e434a05fef4c32ef
m0g13r <155771712+m0g13r@users.noreply.github.com>
2026-02-24 12:06:17 +0100

    Add parser classes for playlist formats supported by mpv

---
+++ playlist_formats.py
@@ -0,0 +1,37 @@
+# Playlist Formats Parser
+
+class M3UParser:
+    """Parser class for M3U files."""
+    def parse(self, file_path):
+        # Implementation for parsing M3U
+        pass
+
+class PLSParser:
+    """Parser class for PLS files."""
+    def parse(self, file_path):
+        # Implementation for parsing PLS
+        pass
+
+class XSPFParser:
+    """Parser class for XSPF files."""
+    def parse(self, file_path):
+        # Implementation for parsing XSPF
+        pass
+
+class CUEParser:
+    """Parser class for CUE files."""
+    def parse(self, file_path):
+        # Implementation for parsing CUE
+        pass
+
+class ASXParser:
+    """Parser class for ASX files."""
+    def parse(self, file_path):
+        # Implementation for parsing ASX
+        pass
+
+class JSONParser:
+    """Parser class for JSON files."""
+    def parse(self, file_path):
+        # Implementation for parsing JSON
+        pass
